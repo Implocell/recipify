fb13dd669d310d9a99b6e67f8235f91877e5c110
Thomas <thomaskgran@gmail.com>
2022-11-20 19:48:28 +0100

    initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOnn9Z0-dsvDikWqTL8Glvfm87rIiUZY0",
+  authDomain: "recipify-ffc0d.firebaseapp.com",
+  projectId: "recipify-ffc0d",
+  storageBucket: "recipify-ffc0d.appspot.com",
+  messagingSenderId: "6799201888",
+  appId: "1:6799201888:web:9ff836e31654d4705cd505"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
